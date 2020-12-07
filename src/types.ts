83c65fdee106553d6ed920e36d6ee9d266f63698
Artem Ignatyev <zazubrik@gmail.com>
2020-12-07 16:02:19 +0300

    Update build artifacts

--- src/types.ts
+++ src/types.ts
@@ -3,10 +3,10 @@ import { components as OCTOKIT_OPENAPI_TYPES } from '@octokit/openapi-types'
 
 export type Octokit = ReturnType<typeof github.getOctokit>
 
-export type CheckSuite = OCTOKIT_OPENAPI_TYPES['schemas']['check-suite']
 export type PullRequest = OCTOKIT_OPENAPI_TYPES['schemas']['pull-request']
 export type Review = OCTOKIT_OPENAPI_TYPES['schemas']['pull-request-review']
-export type Repo = OCTOKIT_OPENAPI_TYPES['schemas']['minimal-repository']
 export type WorkflowRun = OCTOKIT_OPENAPI_TYPES['schemas']['workflow-run']
+export type CheckSuite = OCTOKIT_OPENAPI_TYPES['schemas']['check-suite']
+export type Repo = OCTOKIT_OPENAPI_TYPES['schemas']['minimal-repository']
 
 export type MergeMethod = 'merge' | 'squash' | 'rebase' | undefined
