1abb59b728b2fc7e47174bea3b3762473da7c9fc
Artem Ignatyev <zazubrik@gmail.com>
2020-12-07 15:55:25 +0300

    Extract finder for matching pull requests

--- src/types.ts
+++ src/types.ts
@@ -5,6 +5,7 @@ export type Octokit = ReturnType<typeof github.getOctokit>
 
 export type PullRequest = OCTOKIT_OPENAPI_TYPES['schemas']['pull-request']
 export type Review = OCTOKIT_OPENAPI_TYPES['schemas']['pull-request-review']
+export type Repo = OCTOKIT_OPENAPI_TYPES['schemas']['minimal-repository']
 export type WorkflowRun = OCTOKIT_OPENAPI_TYPES['schemas']['workflow-run']
 
 export type MergeMethod = 'merge' | 'squash' | 'rebase' | undefined
