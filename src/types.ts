2e7ebb283fecf38bfca5a4b4cc810d77bf53d4d5
Artem Ignatyev <zazubrik@gmail.com>
2020-12-07 15:56:21 +0300

    Add support for check_suite events

--- src/types.ts
+++ src/types.ts
@@ -3,6 +3,7 @@ import { components as OCTOKIT_OPENAPI_TYPES } from '@octokit/openapi-types'
 
 export type Octokit = ReturnType<typeof github.getOctokit>
 
+export type CheckSuite = OCTOKIT_OPENAPI_TYPES['schemas']['check-suite']
 export type PullRequest = OCTOKIT_OPENAPI_TYPES['schemas']['pull-request']
 export type Review = OCTOKIT_OPENAPI_TYPES['schemas']['pull-request-review']
 export type Repo = OCTOKIT_OPENAPI_TYPES['schemas']['minimal-repository']
