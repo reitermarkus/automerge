264dc3053c4e4cbafa0b8890fe746f3850e339f0
Artem Ignatyev <zazubrik@gmail.com>
2021-02-01 18:45:08 +0300

    Support commit statuses as check results

--- src/types.ts
+++ src/types.ts
@@ -8,6 +8,7 @@ export type CheckSuite = OCTOKIT_OPENAPI_TYPES['schemas']['check-suite']
 export type PullRequest = OCTOKIT_OPENAPI_TYPES['schemas']['pull-request']
 export type Review = OCTOKIT_OPENAPI_TYPES['schemas']['pull-request-review']
 export type Repo = OCTOKIT_OPENAPI_TYPES['schemas']['minimal-repository']
+export type CommitStatus = OCTOKIT_OPENAPI_TYPES['schemas']['simple-commit-status']
 export type WorkflowRun = OCTOKIT_OPENAPI_TYPES['schemas']['workflow-run']
 
 export type MergeMethod = 'merge' | 'squash' | 'rebase' | undefined
